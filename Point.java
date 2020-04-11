f754d05d5b0a12248aa0e7793ca93a1b35896624
Tea Kocova <teodorakocova@gmail.com>
2020-04-11 16:59:05 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -6,17 +6,75 @@ class Point {
     String boja = "zolta"
 
 	//TODO constructor
+    public Point(String id, double x, double y, String boja) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.boja = boja;
+    }
 
 	//TODO setters and getters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getBoja() {
+        return boja;
+    }
+
+    public void setBoja(String boja) {
+        this.boja = boja;
+    }
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+        x = getX();
+        y = getY();
+        //ako za Y se dobie parametar L ili R ne treba nisto da pravi ne moze y koordinta da se pomesi levo ili desno
+        //isto i za X ako se dobie parametar U ii D nema da moze da odi gore
+        //zatoa imam 4 if-ovi pa vo zavisnost od niv kje se pomesi soodvetnata koordinata
+        if (xDirection == "L") {
+            x = x - 1;
+            setX(x);
+        }
+        if (xDirection == "R") {
+            x = x + 1;
+            setX(x);
+        }
+        if (yDirection == "U") {
+            y = y + 1;
+            setY(y);
+        }
+        if (yDirection == "D") {
+            y = y - 1;
+            setY(y);
+        }
 	}
 
 	public void draw () {
 		//TODO
+        x = getX();
+        y = getY();
+        System.out.printf("x: %.2f y: %.2f", x, y);
 	}
-
-
-
 }
